bc8150aa1d857358c6bb993447dd11b9757ebbb7
Sam <samuelnocita@gmail.com>
2023-12-09 16:18:59 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,23 @@
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+pub mod utils;
 
+//to send different variants of messages
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+pub enum Client{
+  Join{
+    chat_name: Arc<String>,
+  },
+  Post {
+    chat_name: Arc<String>,
+    message: Arc<String>,
+  }
+}
+
+pub enum Server {
+  Message {
+    chat_name: Arc<String>,
+    message: Arc<String>,
+  },
+  Error(String)
+}
