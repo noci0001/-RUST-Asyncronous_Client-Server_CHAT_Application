ac39abc9a002d3ada24d7d790df97924535dd076
Sam <samuelnocita@gmail.com>
2023-12-10 15:20:12 +0100

    Update main.rs

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -1,1 +1,39 @@
+use async_std::{tasks, net};
+use chat::utils::ChatResult;
+use std::sync::Arc;
+use async_std::prelude::*;
 
+mod connection;
+mod chats;
+mod chats_map;
+
+use connection::handle;
+
+fn log_error(result: ChatResult<()>){
+  if let Err(error) = result {
+    println!("Error: {}", error);
+  }
+}
+
+fn main() -> ChatResult<()> {
+  let addr = std::env::args().nth(1).expect("server ADDRESS");
+
+  let chat_table = Arc::new(chat_map::ChatTracker::new());
+
+  async_std::task::block_one(async {
+    let listener = net::TcpListener(addr).await?;
+
+    let mut new_connections = listener.incoming();
+
+    while let Some(socket_result) = new_connections.next().await {
+      let socket = socket_result?;
+
+      let chats = chat_table.clone();
+
+      task::spawn(async {
+        log_error(handle(socket, chats).await);
+      });
+    }
+    Ok(())
+  })
+}
